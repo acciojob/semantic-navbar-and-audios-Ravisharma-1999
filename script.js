4e510db950ceb9891cc743d186b9ce3780e1c94c
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-05 21:49:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-describe('Audio Section Test', () => {
-    it('should display 3 audios with correct sources and controls', () => {
-        cy.visit('/path/to/your/page'); // Replace with the actual path to your page
+() => {
+  // Wait for the page to load or for elements to become available
+  cy.contains('3 random audios').should('be.visible');
 
-        // Assert the heading
-        cy.contains('3 random audios');
+  // Check for audio elements and their attributes
+  cy.get('audio').should('have.length', 3).each(($audio, index) => {
+    expect($audio).to.have.attr('controls');
+    const expectedSrcs = [
+      'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
+      'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
+      'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3'
+    ];
+    expect($audio.attr('src')).to.equal(expectedSrcs[index]);
+  });
+}
 
-        // Assert there are 3 audio elements
-        cy.get('audio').should('have.length', 3);
 
-        // Assert each audio element has the 'controls' attribute and correct source
-        cy.get('audio').each(($audio, index) => {
-            cy.wrap($audio).should('have.attr', 'controls');
-            cy.wrap($audio).should('have.attr', 'src').and('equal', [
-                'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
-                'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
-                'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3'
-            ][index]);
-        });
-    });
-});
